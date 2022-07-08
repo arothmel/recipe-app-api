cdabf03ceb904f7808389a8726424904b11c946a
arothmel <arothmel@gmail.com>
2022-07-08 17:56:36 -0300

    Command stub tested

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -3,11 +3,12 @@ Test custom Django management commands.
 """
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2OpError
+from psycopg2 import OperationalError as Psycopg2Error
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
+import psycopg2
 
 
 @patch('core.management.commands.wait_for_db.Command.check')
@@ -21,3 +22,15 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(database=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for DB when getting op error"""
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
+        
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(database=['default'])
+
