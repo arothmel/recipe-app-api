2c6826db6291b22360cc3db44c92238821c98ffc
arothmel <arothmel@gmail.com>
2022-07-08 15:22:51 -0300

    add psycopg2 adaptor for PostgreSQL db

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+
+
+def subtract(x, y):
+    """Add x and y and return result."""
+    r = abs(x - y)
+    return r
