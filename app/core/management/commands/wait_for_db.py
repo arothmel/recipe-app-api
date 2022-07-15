58b816e01fa4eb7d0ed4f2977ad613094da51539
arothmel <arothmel@gmail.com>
2022-07-15 15:11:15 -0300

    Deb is available!

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -13,4 +13,14 @@ class Command(BaseCommand):
     """Django command to wait for database."""
 
     def handle(self, *args, **options):
-        pass
+        """Entrypoint for command"""
+        self.stdout.write('Just waiting for a friend named DB...')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except(Psycopg2OpError, OperationalError):
+                self.stdout.write('Still waiting for Deb, one more second...')
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS('Deb is available!'))
